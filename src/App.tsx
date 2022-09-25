b5162b4471fd9f7fb2988013b0dbc650d3d940e8
jeyefendi <jeyhunfarajov@gmail.com>
2022-09-26 00:49:51 +0300

    context routering project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,18 @@
-import { CreateProduct } from "./components/CreateProduct";
-import { ErrorMessage } from "./components/ErrorMessage";
-import { Loader } from "./components/Loader";
-import { Modal } from "./components/Modal";
-import { IProduct } from './models'
-import { Product } from "./components/Product";
-import { useProducts } from "./hooks/products";
-import {useState} from 'react'
+import { Route, Routes } from "react-router-dom";
+import { ProductsPage } from "./pages/ProductsPage";
+import { AboutPage } from "./pages/AboutPage";
+import { Navigation } from "./components/Navigation";
 
 function App() {
-  const {loading, error, products, addProduct} = useProducts()
-  const [modal, setModal] = useState(true)
-
-  const createHandler = (product: IProduct) => {
-    setModal(false)
-    addProduct(product)
-  }
-
   return (
     <>
-    <div className="container mx-auto max-w-2xl pt-5">
-      { loading && <Loader/> }
-      { error && <ErrorMessage error={error} /> }
-      { products.map(product => <Product product={product} key={product.id} />) }
-
-    {modal && <Modal title="Create new product" onClose={() => setModal(false)}>
-      <CreateProduct onCreate={createHandler}/>
-    </Modal>}
-    </div>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
